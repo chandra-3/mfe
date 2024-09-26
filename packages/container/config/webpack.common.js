4db86ab827c90f9f2af4303fb5b53f40dac231b1
chandra-3 <162978794+chandra-3@users.noreply.github.com>
2024-09-26 15:16:57 +0530

    created container deploy script

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,3 +1,6 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
 module.exports = {
     module: {
         rules: [
@@ -16,4 +19,9 @@ module.exports = {
 
         ],
     },
+    plugins : [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+    ],
 };
